64872e3de0f3c78371e56057793d9dee6084c3a0
thymeseed <thymeseed@126.com>
2015-11-02 22:41:13 +0800

    提交第一次代码

---
+++ Bulls and Cows/Bulls and Cows/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "请输入a，b的值" << endl;
+	cin >> a >> b;
+	cout << "输出a的值" << a << "输出b的值" << b << endl;
+	system("pause");
+	return 0;
+}
